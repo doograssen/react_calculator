a0f0d0f9801425eee7c71094855cc1cbf8b2e67f
doograssen <rythmag@gmail.com>
2024-01-14 22:53:42 +0300

    adding component style

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,13 +1,13 @@
-import { CalcButton } from './CalcButton';
 import { useState } from 'react';
-import './calculator.css';
+import styles from './calculator.module.css';
 
 export const Calculator = (props) => {
-	const OPERATIONS = ['+', '-', '*', '/', '.'];
-	let [result, setResult] = useState("");
+	const OPERATIONS = ['+', '-', '*', '/'];
+	let [result, setResult] = useState('');
+  let [expression, setOperation] = useState({});
 
   const handleClick = (e) => {
-    if(result.length >=16){
+    if (result.length >= 16) {
       setResult("!So Much Big Input");
       return;
     }
@@ -17,47 +17,55 @@ export const Calculator = (props) => {
 		console.log(e.target);
     setResult(result.concat(e.target.textContent));
   }
-
+  const operationChoose = (e) => {
+    if (!expression.hasOwnProperty('operation')) {
+      setResult('');
+      setOperation({ ...expression, firstArgument: result, operation: e.target.textContent});
+    }
+    else {
+      setResult('');
+      setOperation({ ...expression, secondArgument: result, result: e.target.textContent});
+    }
+  }
   const clear = () => {
     setResult("");
   }
   const backSpace = () => {
     setResult(result.slice(0, result.length - 1))
   }
-
   const calculate = () => {
     try {
-      result=eval(result).toString()
-      if(result.includes('.')){
+      result = eval(result).toString();
+      if (result.includes('.')) {
         result =+ eval(result);
         result = result.toFixed(4).toString();
         setResult(result);
-      }else{
+      } else {
         setResult(eval(result).toString());
       }
-
     } catch (err) {
       setResult("Error");
     }
-
   }
 	return (
-		<div className="calc">
-			<div className="calc__input">
-				<input type="text" value={result} />
+		<div className={styles.calc}>
+			<div className={styles.screen}>
+				<input type="text" value={result}  className={styles.field} />
 			</div>
-			<div className="calc__numbers">
-				{[...Array(10).keys()].map((item) => (
-					<button onClick={handleClick}>{item}</button>
-				))}
-			</div>
-			<div className="calc__operations">
-				<button onClick={clear} className="clear color">Clear</button>
-        <button onClick={backSpace} className="backspace color">C</button>
-				{OPERATIONS.map((item) => (
-					<button onClick={handleClick}>{item}</button>
-				))}
-        <button onClick={calculate} className="equal color">=</button>
+			<button onClick={calculate} className={styles.btn + ' ' + styles.operator}>=</button>
+			<div className={styles.panel}>
+				<div className={styles.numbers}>
+					{[...Array(10).keys()].reverse().map((item) => (
+						<button onClick={handleClick} className={styles.btn}>{item}</button>
+					))}
+					<button onClick={clear} className={styles.btn + ' ' + styles.operator}>C</button>
+					<button onClick={backSpace} className={styles.btn + ' ' + styles.operator}>.</button>
+				</div>
+				<div className={styles.operatorTab}>
+					{OPERATIONS.map((item) => (
+						<button onClick={operationChoose} className={styles.btn + ' ' + styles.operator}>{item}</button>
+					))}
+				</div>
 			</div>
 		</div>
 	);
